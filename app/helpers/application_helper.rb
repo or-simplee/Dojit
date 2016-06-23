ec9291aa683c119fa531be8e2d528530d70a8ea7
or-simplee <or@simplee.com>
2016-06-23 15:48:20 +0300

    change form_group assignment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,11 @@ module ApplicationHelper
     'Or Bar-Tal'
   end
 
-  def form_group_tag(errors, &block)
+  def form_group_tag(errors)
     if errors.any?
-      content_tag :div, capture(&block), class: 'form-group has-error'
+      "form-group has-error"
     else
-      content_tag :div, capture(&block), class: 'form-group'
+      "form-group"
     end
   end
 end

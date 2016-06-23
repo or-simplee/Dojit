6b71c20699398a86ff300d0d038a405e273e20eb
or-simplee <or@simplee.com>
2016-06-23 14:48:40 +0300

    validating posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
   default_scope { order('created_at DESC')}
+
+  validates :title, length: {minimum: 5, message:"Title has to be minimum 5 characters"}
+  validates :body, length: {minimum: 20}
+  validates :topic, presence: true
+  validates :user, presence: true
 end

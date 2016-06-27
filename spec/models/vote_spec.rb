4025671ac785a6f653bd4848456ce548736bb3a2
or-simplee <or@simplee.com>
2016-06-27 11:53:45 +0300

    rspec install. vote and post tests

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe Vote do
+  describe "validations" do
+
+    before do
+      v1 = Vote.new(value: 1)
+      v2 = Vote.new(value: -1)
+      v2 = Vote.new(value: 2)
+    end
+
+    describe 'value validation' do
+      it "only allows -1 or 1 as values" do
+        expect( v1.valid? && v2.valid? && not v3.valid? ).to eq(true)
+      end
+    end

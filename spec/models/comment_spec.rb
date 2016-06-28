f44fa452ddc39019a2eda46ea28d65878b085154
or-simplee <or@simplee.com>
2016-06-28 17:36:09 +0300

    redirect after login, reformatting arrows, and sending emails premissions

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -12,22 +12,40 @@ describe Comment do
       @comment = Comment.new(body: 'My comment', post: @post, user_id: 10000)
     end
 
-    it "sends an email to users who have favorited the post" do
-      @user.favorites.where(post: @post).create
+    # We don't need to change anything for this condition;
+    # The email_favorites attribute defaults to true
+      context "with user's permission" do
 
-      allow( FavoriteMailer )
-        .to receive(:new_comment)
-        .with(@user, @post, @comment)
-        .and_return( double(deliver: true) )
+      it "sends an email to users who have favorited the post" do
+        @user.favorites.where(post: @post).create
 
-      @comment.save
+        allow( FavoriteMailer )
+          .to receive(:new_comment)
+          .with(@user, @post, @comment)
+          .and_return( double(deliver: true) )
+
+        @comment.save
+      end
+
+      it "does not send emails to users who haven't" do
+        expect( FavoriteMailer )
+          .not_to receive(:new_comment)
+
+        @comment.save
+      end
     end
+    context "without permission" do
 
-    it "does not send emails to users who haven't" do
-      expect( FavoriteMailer )
-        .not_to receive(:new_comment)
+      before { @user.update_attribute(:email_favorites, false) }
 
-      @comment.save
+      it "does not send emails, even to users who have favorited" do
+        @user.favorites.where(post: @post).create
+
+        expect( FavoriteMailer )
+          .not_to receive(:new_comment)
+
+        @comment.save
+      end
     end
   end
 end
